beddd55782df0fd4d73caa14f5eb3531d2407102
tyler knapp <tgknapp11@gmail.com>
2019-06-26 17:39:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,4 @@
   def find_element_index(array, value_to_find)
-  #   array.length.times do |i|
-  #     if array[i] == value_to_find
-  #       return i
-  #     end
-  # end
-  # nil
   return array.index(value_to_find)
 end
 
